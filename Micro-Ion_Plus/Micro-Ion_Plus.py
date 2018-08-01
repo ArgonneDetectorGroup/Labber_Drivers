a8b447f606bfda0bddc9733466a0e0a0a0351ab3
Faustin Carter <faustin.carter@gmail.com>
2018-08-01 13:54:32 -0500

    Add pressure gauge driver

---
+++ Micro-Ion_Plus/Micro-Ion_Plus.py
@@ -0,0 +1,44 @@
+LABBER_PATH = '/Program Files (x86)/Labber/Script/'
+LABBER_SERVER = 'localhost'
+VOLTMETER = 'Stanford Research SIM 970 Voltmeter'
+VOLTMETER_QUANT = 'Ch4 Voltage'
+
+import sys
+sys.path.append(LABBER_PATH)
+
+import Labber as lab
+import InstrumentDriver
+
+def VtoP(voltage):
+    return 10**(2*(voltage-5.5))
+
+class Driver(InstrumentDriver.InstrumentWorker):
+    
+    def performOpen(self, options={}):
+        """Try to connect to the voltmeter. Start it up if necessary."""
+        self.voltmeter = None
+
+        self.client = lab.connectToServer(LABBER_SERVER)
+        instruments = self.client.getListOfInstruments()
+
+        for hardware, config in instruments:
+            if hardware == VOLTMETER:
+                self.voltmeter = self.client.connectToInstrument(VOLTMETER, dict(name=config['name']))
+
+        assert self.voltmeter is not None, "ERROR: Could not find a voltmeter!"
+
+        if not self.voltmeter.isRunning():
+            self.voltmeter.startInstrument()
+
+        assert self.voltmeter.isRunning() == True, "ERROR: Cannot start voltmeter"
+
+    def performGetValue(self, quant, options={}):
+        if quant.name == 'Pressure':
+            voltage = self.voltmeter.getValue(VOLTMETER_QUANT)
+            pressure = VtoP(voltage)
+
+            return pressure
+        else:
+            assert False, "Error: unknown quantity"
+
+        return 'none'
