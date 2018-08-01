8aa0d868f6500947d85e1b5953b6d2dccf25eb47
Faustin Carter <faustin.carter@gmail.com>
2018-08-01 11:40:11 -0500

    Add sim970 driver

---
+++ SRS_SIM970/SIM970_Driver.py
@@ -0,0 +1,13 @@
+#Add the parent directory to the path to expose common_lib
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+import sys
+sys.path.append(parent_dir)   
+
+from common_lib.SIM9XX_Driver import SIM9XX_Driver
+
+class Driver(SIM9XX_Driver):
+    """ This class re-implements the SIM9XX driver"""
+
+    pass
